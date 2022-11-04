2a6dc388088f65838460743fb06ec67accaacfd0
Sacaar Jain <jainsacaar@gmail.com>
2022-11-03 18:50:39 -0700

    model and controller courses.js built

---
+++ backend/src/models/courses.js
@@ -0,0 +1,15 @@
+const database = require("./database.js");
+
+const courses = {
+    async getAllCourses() {
+        const dbo = await database.getDbo();
+        return await dbo.collection("courses").find().toArray();
+    },
+
+    async createCourse(course) {
+        const dbo = await database.getDbo();
+        return await dbo.collection("courses").insertOne(course);
+    }
+}
+
+module.export = courses;
