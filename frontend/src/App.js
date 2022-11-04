562239150f78df7d85f6278c380f48d1dd407aad
benguo2003 <benguo2003731@gmail.com>
2022-11-03 18:10:57 -0700

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,45 @@
+import React from 'react';
 import './App.css';
-import Box from '@mui/material/Box';
 
-function App() {
-    return (
-        <div>
-            <div className="title">
-                MyCourse
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          teacher: '',
+          courseid: 0,
+          coursename: '',
+          days: '',
+          times: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({teacher: event.target.value});
+      }
+    
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.teacher);
+        event.preventDefault();
+    }
+
+    render() {
+        return(
+            <div>
+                <div className="title">
+                    MyCourse
+                </div>
+                <form onSubmit={this.handleSubmit} className="submitForm">
+                    <label>
+                    Teacher Name:
+                    <input type="text" value={this.state.teacher} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
-        </div>
-    );
+        )
+    }
 }
+
 export default App;
